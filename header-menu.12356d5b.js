0a36b45740411f7eea245b5d3befaa90139b98d5
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-06-04 20:00:10 +0000

    Deploying to gh-pages from @ Morelli01/Javascript-team2-project@96376ab69ba03ee855d94b01f1ac89110d437cab 🚀

--- header-menu.12356d5b.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("fXFzE",(function(e,n){var d=document.getElementById("btn-menu"),o=document.querySelector("header");d.addEventListener("click",(function(){o.classList.toggle("open")}))}));
-//# sourceMappingURL=header-menu.12356d5b.js.map
--- header-menu.a98a7c0c.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("N3qRZ",(function(e,n){const o=document.getElementById("btn-menu"),t=document.querySelector("header");o.addEventListener("click",(function(){t.classList.toggle("open")}))}));
-//# sourceMappingURL=header-menu.a98a7c0c.js.map
