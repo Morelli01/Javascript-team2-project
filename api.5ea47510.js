3018979fe449238844ed744ca46016293b3ed933
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-06-05 09:58:33 +0000

    Deploying to gh-pages from @ Morelli01/Javascript-team2-project@7ae4cd15a51151b3f34f9f3705e91750f1dc9694 🚀

--- api.5ea47510.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("dK6vY",(function(e,d){}));
-//# sourceMappingURL=api.5ea47510.js.map
--- api.602a1fcc.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("8LhE9",(function(e,n){}));
-//# sourceMappingURL=api.602a1fcc.js.map
