e50cc5ce4505e309d2c517302402a3110f63ae6f
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-06-06 20:13:32 +0000

    Deploying to gh-pages from @ Morelli01/Javascript-team2-project@a6d39e359002943390314f2fd699267b5263ac76 🚀

--- hero.80087c37.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("2ufDr",(function(e,f){}));
-//# sourceMappingURL=hero.80087c37.js.map
