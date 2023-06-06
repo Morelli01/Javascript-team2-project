8afdabf9d6b3ee133166e80c2a7fec3629f4ab2d
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-06-06 12:38:30 +0000

    Deploying to gh-pages from @ Morelli01/Javascript-team2-project@278674a1ce9589382e02157467c3c45660b151cc 🚀

--- spin.87d081ef.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("dpMC2",(function(e,d){}));
-//# sourceMappingURL=spin.87d081ef.js.map
--- spin.b72ba849.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("dBKQr",(function(e,d){}));
-//# sourceMappingURL=spin.b72ba849.js.map
