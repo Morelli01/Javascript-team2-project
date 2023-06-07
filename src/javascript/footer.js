fcffec9f1858a9cd34ae60d5f5ee32c9472c2853
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-06-07 23:04:29 +0200

    Footert

--- src/javascript/footer.js
+++ src/javascript/footer.js
@@ -1,10 +1,10 @@
 import Splide from '@splidejs/splide';
 import { Grid } from '@splidejs/splide-extension-grid';
+
 import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css';
 
 const teamSplide = new Splide('.splide', {
   type: 'slide',
-  // perPage: 4,
   rewind: false,
   height: '290px',
   width: '900px',
@@ -27,11 +27,11 @@ const teamSplide = new Splide('.splide', {
       direction: 'ttb',
       perPage: 2,
       perMove: 1,
-      height: 650,
+      height: '650px',
       grid: false,
     },
-    1279: {
-      height: '210px',
+    1280: {
+      height: '300px',
       width: '700px',
     },
   },
@@ -39,31 +39,31 @@ const teamSplide = new Splide('.splide', {
 teamSplide.mount({ Grid });
 
 const teamModalLink = document.querySelector('.team-modal-link');
-const modal = document.querySelector('.data-modal-team');
-const modalCloseBtn = document.querySelector('.modal-team__close-btn');
+const modalTeam = document.querySelector('.data-modal-team');
+const modalCloseBtn = document.querySelector('.modal-team-close-btn');
 
 teamModalLink.addEventListener('click', teamModalShow);
 modalCloseBtn.addEventListener('click', onCloseBtnClick);
-modal.addEventListener('click', onBackdropClick);
+modalTeam.addEventListener('click', onBackdropClick);
 
-function teamModalShow(evt) {
-  evt.preventDefault();
-  modal.classList.toggle('open');
+function teamModalShow(e) {
+  e.preventDefault();
+  modalTeam.classList.toggle('open');
   document.addEventListener('keydown', onEscModalTeam);
 }
-function onEscModalTeam(evt) {
-  if (evt.code === 'Escape') {
+function onEscModalTeam(e) {
+  if (e.code === 'Escape') {
     onCloseBtnClick();
     document.removeEventListener('keydown', onEscModalTeam);
   }
 }
 function onCloseBtnClick() {
-  modal.classList.toggle('open');
+  modalTeam.classList.toggle('open');
   document.body.classList.toggle('body--modal-open');
 }
 
-function onBackdropClick(evt) {
-  if (evt.target.classList.contains('popup-body')) {
+function onBackdropClick(e) {
+  if (e.target.classList.contains('popup-body')) {
     onCloseBtnClick();
   }
 }
