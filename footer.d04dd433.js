c8ea66b2f7506a09daaf90760d78fe0195eef674
Morelli01 <120120074+Morelli01@users.noreply.github.com>
2023-06-07 21:46:24 +0000

    Deploying to gh-pages from @ Morelli01/Javascript-team2-project@fb08009f62d1d968d2b7a1db5d1f4182aabc10e7 🚀

--- footer.d04dd433.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("eWcFN",(function(e,n){}));
-//# sourceMappingURL=footer.d04dd433.js.map
